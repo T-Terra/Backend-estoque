e00ee4337611961896ea61bf13d77fa34ee58eca
T-Terra <gabst138@gmail.com>
2025-03-07 17:00:27 -0300

    feat: add model peca

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models.peca import peca
 
 # Register your models here.
+admin.site.register(peca)
---
+++ api/models/peca.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+
+# Create your models here.
+class peca(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=300)
+    code = models.CharField(max_length=120, unique=True)
+    amount = models.IntegerField(
+        validators=[MinValueValidator(0)]
+    )
+
+    class Meta:
+        verbose_name = 'Peça'
+        verbose_name_plural = 'Peças'
+
+    def __str__(self):
+        return self.name
