d98f2939f5e2951c6ab0cfa6e96a7df3db334564
T-Terra <gabst138@gmail.com>
2025-03-12 17:01:47 -0300

    fix: bad req

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://frontend-estoque-delta.vercel.app"]
+ALLOWED_HOSTS = [
+    "http://localhost:5173",  # Se estiver rodando React localmente
+    "http://127.0.0.1:5173",
+    "https://frontend-estoque-delta.vercel.app"
+]
 
 APPEND_SLASH = False
 
