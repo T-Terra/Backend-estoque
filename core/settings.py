8545b792c3f0e3e13f514455d3b56f83d4dbd2fc
T-Terra <gabst138@gmail.com>
2025-03-12 14:25:35 -0300

    fix: deploy

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -105,7 +105,7 @@ DATABASES = {
     }
 }
 
-data_base_host = os.getenv("DB_HOST")
+data_base_host = os.environ.get("DB_HOST")
 DATABASES["default"] = dj_database_url.parse(data_base_host)
 
 # Password validation
