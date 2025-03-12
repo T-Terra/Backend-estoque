68d6724bb3e9b0b4146cd245f77450febd1fec96
T-Terra <gabst138@gmail.com>
2025-03-12 13:35:32 -0300

    feat: add create user default and dockerfile

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Usar uma imagem oficial do Python 3.12
+FROM python:3.12
+
+# Criar diretório de trabalho
+WORKDIR /
+
+# Copiar arquivos do projeto para dentro do container
+COPY . /
+
+# Instalar Poetry
+RUN pip install poetry
+
+# Instalar dependências do projeto
+RUN poetry install --no-root
+
+RUN poetry run python manage.py migrate
+
+# Comando para rodar o app (mude conforme necessário)
+# CMD ["poetry", "run", "python", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD poetry run python create_superuser.py || true && poetry run gunicorn --bind 0.0.0.0:8000 core.wsgi:application
---
+++ create_superuser.py
@@ -0,0 +1,26 @@
+import os
+from django.contrib.auth import get_user_model
+from dotenv import load_dotenv
+
+# Carregar variáveis de ambiente
+load_dotenv(override=True)
+
+# Certificar-se de que o Django está configurado corretamente
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Importar a configuração do Django (após setar o DJANGO_SETTINGS_MODULE)
+import django
+django.setup()
+
+User = get_user_model()
+
+# Obter as variáveis de ambiente
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin")
+
+# Criar o superuser se não existir
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email='', password=password)
+    print(f"Superuser {username} criado com sucesso!")
+else:
+    print(f"Superuser {username} já existe.")
