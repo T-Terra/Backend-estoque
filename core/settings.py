fa197c32deb2c0c9032e52711525dc4807af6c6e
T-Terra <gabst138@gmail.com>
2025-03-21 12:46:49 -0300

    fix: test v8

--- core/settings.py
+++ core/settings.py
@@ -121,20 +121,15 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 CI = os.getenv("CI", "ci").lower() == "ci"
 
-if CI == "ci":
-    database_host = os.getenv('POSTGRES_HOST')
-
-    if not database_host:
-        raise ValueError("POSTGRES_HOST não foi encontrada no ambiente!")
-    
+if CI == "ci":    
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('POSTGRES_DB'),
-            'USER': os.getenv('POSTGRES_USER'),
-            'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-            'HOST': os.getenv('POSTGRES_HOST'),
-            'PORT': os.getenv('POSTGRES_PORT'),
+            'NAME': os.getenv('POSTGRES_DB', 'test_db'),
+            'USER': os.getenv('POSTGRES_USER', 'test_user'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'test_password'),
+            'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+            'PORT': os.getenv('POSTGRES_PORT', 5432),
         }
     }
 else:
